f9be9ad765a5aecfb3b238b4d242a989865d6f36
Brian Mosley <private address>
2021-07-21 21:25:53 -0400

    Implemented map-based services.  Closes #6.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+}
