48f71a65b10c24e19e04b7e772a3f05f1d3457d4
Brian Mosley <private address>
2021-07-21 20:48:10 -0400

    Refactored service interfaces to base interface.  Closes #11.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
-import javax.persistence.Id;
 import java.io.Serializable;
 
-@Id
 public class BaseEntity implements Serializable {
 
     private Long id;
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetService.java
@@ -4,10 +4,4 @@ import guru.springframework.sfgpetclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
-}
+public interface VetService extends CrudService<Vet, Long> { }
